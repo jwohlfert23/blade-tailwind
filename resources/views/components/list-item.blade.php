08411c85c51a06f3d93969bc370cef5724b10b16
Jack Wohlfert <jack.wohlfert@gmail.com>
2021-04-29 08:52:56 -0400

    Better spacing for list items

--- resources/views/components/list-item.blade.php
+++ resources/views/components/list-item.blade.php
@@ -7,7 +7,7 @@
         class="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out"
     >  @endif
         <div class="flex items-center px-4 py-4 sm:px-6">
-            <div class="min-w-0 flex-1 flex items-center">
+            <div class="min-w-0 flex-1 flex items-center space-x-4">
 
                 @if(!empty($avatar))
                     <div class="flex-shrink-0">
@@ -15,13 +15,13 @@
                     </div>
                 @endif
 
-                <div class="min-w-0 flex-1 px-4">
+                <div class="min-w-0 flex-1">
                     <div>
                         {{$primary}}
                     </div>
 
                     @if(!empty($secondary))
-                        <div class="hidden md:block">
+                        <div class="text-gray-500">
                             {{$secondary}}
                         </div>
                     @endif
