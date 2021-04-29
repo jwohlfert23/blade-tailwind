d401219087697d1ef5a202c65c519f2eeac9eb3f
Jack Wohlfert <jack.wohlfert@gmail.com>
2021-04-29 09:02:27 -0400

    Merge attributes for list element

--- resources/views/components/list.blade.php
+++ resources/views/components/list.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-white border shadow-sm overflow-hidden sm:rounded-md">
+<div {{$attributes->merge(['class' => 'bg-white border shadow-sm overflow-hidden sm:rounded-md'])}}>
     <ul>
         {{$slot}}
     </ul>
