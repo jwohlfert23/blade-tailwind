55ec644198ebd339df07f340ab0392a1cbd508d5
Jack Wohlfert <jack.wohlfert@gmail.com>
2021-04-29 12:07:03 -0400

    Adjustable modal width

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -31,7 +31,7 @@
             x-transition:leave="ease-in duration-200"
             x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
             x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6"
+            class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:{{$width ?? 'max-w-sm'}} sm:w-full sm:p-6"
         >
             @if(!empty($title))
                 <h3 class="text-lg leading-6 font-medium text-gray-900 mb-6">
