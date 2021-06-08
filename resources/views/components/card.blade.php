95ef2c32464ac5b7d565f2cf0aafd38abf5510e4
Jack Wohlfert <jack.wohlfert@gmail.com>
2021-06-08 12:17:43 -0400

    Card padding options

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,8 +1,10 @@
+<?php $padding = $padding ?? 'px-5 py-3'; ?>
+
 <div>
     @if(isset($label))
         <div class="text-xs font-semibold text-gray-500 -ml-1 mb-1 tracking-wide uppercase">{{$label}}</div>
     @endif
-    <div {{$attributes->merge(['class' => "rounded bg-white shadow-sm border overflow-hidden px-5 py-3"])}}>
+    <div {{$attributes->except('padding', 'label')->merge(['class' => "rounded bg-white shadow-sm border overflow-hidden $padding"])}}>
         {{$slot}}
     </div>
 </div>
